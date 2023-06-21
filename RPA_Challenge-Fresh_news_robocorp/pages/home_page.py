257ae81dc84282391e5f3146de22d1ed251a041a
vfil <wladyslawfil@gmail.com>
2023-06-21 17:46:58 +0300

    Refactore home_page code

--- RPA_Challenge-Fresh_news_robocorp/pages/home_page.py
+++ RPA_Challenge-Fresh_news_robocorp/pages/home_page.py
@@ -1,23 +1,15 @@
-
 # import RPA modules
 from RPA.Browser.Selenium import Selenium
-
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.wait import WebDriverWait
 # import custom modules
 import constants as const
 
 
 class HomePage:
-    SEARCH_BUTTON = (By.CSS_SELECTOR, '[data-test-id="search-button"]')
-    SEARCH_FIELD = (By.CSS_SELECTOR, '[data-testid="search-input"]')
-    SUBMIT_BUTTON = (By.CSS_SELECTOR, '[data-test-id="search-submit"]')
-
-    SEARCH_TEXTFIELD = (By.ID, 'searchTextField')
 
     def __init__(self, browser_lib: Selenium):
         self.browser_lib = browser_lib
 
+    # Navigate
     def lend_first_page(self):
         self.browser_lib.open_available_browser(const.BASE_URL)
         title = self.browser_lib.get_title()
@@ -27,22 +19,25 @@ class HomePage:
     # Search query
     def enter_search_query(self, query):
         try:
+            # define selectors
+            search_button = 'css:[data-test-id="search-button"]'
+            search_input = 'css:[data-testid="search-input"]'
+            search_submit = 'css:[data-test-id="search-submit"]'
+            searchTextField = 'searchTextField'
+
             # Type search query
-            self.browser_lib.click_element(
-                'css:[data-test-id="search-button"]')
+            self.browser_lib.click_element(search_button)
             self.browser_lib.input_text_when_element_is_visible(
-                'css:[data-testid="search-input"]', query)
-            self.browser_lib.click_element(
-                'css:[data-test-id="search-submit"]')
+                search_input, query)
+            self.browser_lib.click_element(search_submit)
 
             # Validate applied search query on the search page
-            searchTextField = 'searchTextField'
             self.browser_lib.wait_until_page_contains_element(searchTextField)
-
             search_text_field_value = self.browser_lib.get_element_attribute(
                 searchTextField, 'value')
             searchTextFieldMatched = self.browser_lib.is_element_attribute_equal_to(
                 searchTextField, 'value', query)
+
             assert searchTextFieldMatched, f"Search text field value [{search_text_field_value}] doesn't match the query [{query}]"
 
         except Exception as e:
